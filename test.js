1f52535b8caa512676cb372301fb30caaa08a0ba
danica <liwandan@gmail.com>
2020-11-11 02:11:15 +1300

    only can select one size

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-function onSelectSize(sizeButton) {
+onSelectSize = (sizeButton) => {
+    var sizeButtons = document.getElementsByClassName("size-button");
+    for (var i = 0; i < sizeButtons.length; i++) {
+        sizeButtons[i].classList.remove("selected-size-button")
+    }
+
     var size = sizeButton.innerHTML;
+    sizeButton.classList.add("selected-size-button");
     document.getElementById("selected-size").innerHTML = size;
+
 }
